79cc8e3d54834bcd05359525ba733c79e13d3ed4
Jason <133819475+Quiet44@users.noreply.github.com>
2023-11-20 13:16:35 -0800

    if and else added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 var button = Document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 
-
+// // When user clicks on the button, function is triggered to show cat image
 button.addEventsListener("click", function () {
+  if (cat.classList.contains("show")) {
+   // console.log("yes");
+  cat.classList.remove("show");
+  button.innerText = "Wait, come back!";
+  button.classList.add("disappeear");
+} else {
+   // console.log("No");
    cat.classList.add("show");
+   button.innerText = "Shoo cat!";
+   button.classList.remove("disappear");
+}
 });
